ac8c45076f45fc2d8b85c3f4b7a1106c7b76e439
Randika Lakmal <randika.help@gmail.com>
2021-02-10 15:51:33 +0530

    After Validation Test Complete
    Validation Check and @DisplayName tag demo

--- src/test/java/com/randikalakmal/Junit5/StudentManagerTest.java
+++ src/test/java/com/randikalakmal/Junit5/StudentManagerTest.java
@@ -1,12 +1,14 @@
 package com.randikalakmal.Junit5;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StudentManagerTest {
 
 
     @Test
+    @DisplayName("Should Create Contact")
     public void shouldCreateStudent() {
         StudentManager studentManager = new StudentManager();
         studentManager.addStudent("Randika", "Lakmal", "0123456789");
@@ -17,5 +19,32 @@ class StudentManagerTest {
                         student.getLastName().equals("Lakmal") &&
                         student.getPhoneNumber().equals("0123456789")));
     }
+
+    @Test
+    @DisplayName("Should Not Create When First Name is Null")
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull(){
+        StudentManager studentManager = new StudentManager();
+        Assertions.assertThrows(RuntimeException.class,()->{
+            studentManager.addStudent(null,"Lakmal","0711596674");
+        });
+    }
+    @Test
+    @DisplayName("Should Not Create When Last Name is Null")
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull(){
+        StudentManager studentManager = new StudentManager();
+        Assertions.assertThrows(RuntimeException.class,()->{
+            studentManager.addStudent("Randika",null,"0711596674");
+        });
+    }
+    @Test
+    @DisplayName("Should Not Create When Phone Number is Null")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull(){
+        StudentManager studentManager = new StudentManager();
+        Assertions.assertThrows(RuntimeException.class,()->{
+            studentManager.addStudent("Randika","Lakmal",null);
+        });
+    }
+
+
 
 }
